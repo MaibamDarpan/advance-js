bc1adfab0b7ecb54031f37b33cdea544001305e5
MaibamDarpan <dopd2870@gmail.com>
2021-06-07 04:02:26 +0100

    added null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+// let bird;
+// console.log(bird);
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }      
+// // NO RETURN VALUE
+// const result = add(12,32);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// // NO RETURN VALUE + PERAMETRE DIDN'T PASS
+
+// const result = add(134);
+// console.log(result);
+
+// const romeo = {name: 'Badshah', phone: 12344};
+// console.log(romeo.juliet);
+
+// let fun = undefined;
+// console.log(fun);
+
+let number = null;
+console.log(number);
