b064d5b3dc8cc5353c1de2c9925883d73b4e0183
MaibamDarpan <dopd2870@gmail.com>
2021-06-07 03:42:30 +0100

    added first js

---
+++ true-false.js
@@ -0,0 +1,63 @@
+// const age = 8;
+// if (age > 4) {
+//     console.log('condition is true');
+    
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+// const age = -5;
+// if (age) {
+//     console.log('conditon is true');
+// } else {
+//     console.log('condition is false');
+    
+// }
+
+// const age = 0;
+// if (age) {
+//     console.log('condition is true');
+// } else {
+//     console.log('condition is false');
+// // }
+// const name = 'Sumit';
+// if (name.length>0) {
+//     console.log('condition is true');
+    
+// } else {
+// //     console.log('condition is false');
+// // }
+// const name = '';
+// if (name) {
+//     console.log('condition is true');
+    
+// } else {
+//     console.log('condition is false');
+    
+// }
+
+// const name = ' ';
+// if (name) {
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+
+// }
+
+
+// // All THE FALSY VALUES
+// const event = false;
+// const num = 0;
+// const friend = '';
+// var value = null;
+// var number = NaN;
+
+
+// // ALL THE TRUTHY VALUES
+// const num = '0';
+// const friend = ' ';
+// const value = [];
+// const object = {};
+// const event = true;
